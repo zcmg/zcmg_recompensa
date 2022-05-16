7663bb652ff035d1fe88c667211d0f5a73deb98f
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2022-05-16 18:05:50 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 Citizen.CreateThread(function()
   while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    TriggerEvent(Config.ESXTrigger, function(obj) ESX = obj end)
     Citizen.Wait(0)
   end
 end)
