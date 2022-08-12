318c8bb9b84cbde5ae1eaaea47b4f53e0bf7d434
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2022-08-12 18:58:38 +0100

    Delete database.sql

--- database.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `zcmg_recompensa`;
-CREATE TABLE `zcmg_recompensa` (
-  `code` text DEFAULT NULL,
-  `type` text DEFAULT NULL,
-  `data1` text DEFAULT NULL, 
-  `data2` text DEFAULT NULL 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
