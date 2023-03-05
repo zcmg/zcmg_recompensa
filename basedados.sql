20ada7ee3b7ac89020ef3e6f9d854b006eb69395
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2023-03-05 22:00:11 +0000

    Add files via upload

--- basedados.sql
+++ basedados.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `zcmg_recompensa` (
   `owner` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
+DROP TABLE IF EXISTS `zcmg_recompensa_admins`;
 CREATE TABLE IF NOT EXISTS `zcmg_recompensa_admins` (
   `identifier` varchar(46) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
